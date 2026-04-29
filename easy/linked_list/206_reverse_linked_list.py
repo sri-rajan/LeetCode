1a8c8604e695480a6c9f659fbe26c10bb13278e0
sri-rajan <sriraja781@gmail.com>
2026-04-29 23:17:35 +0530

    Solved 206 Reverse Linked List

---
+++ easy/linked_list/206_reverse_linked_list.py
@@ -0,0 +1,80 @@
+# 206. Reverse Linked List
+# Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+# Example 1:
+
+
+# Input: head = [1,2,3,4,5]
+# Output: [5,4,3,2,1]
+# Example 2:
+
+
+# Input: head = [1,2]
+# Output: [2,1]
+# Example 3:
+
+# Input: head = []
+# Output: []
+
+
+# Constraints:
+
+# The number of nodes in the list is the range [0, 5000].
+# -5000 <= Node.val <= 5000
+
+
+# Follow up: A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+
+def reverseList(head):
+    temp = head
+    listValues = []
+    while temp:
+        listValues.append(temp.val)
+        temp = temp.next
+    finalList = head
+    for i in range(len(listValues) - 1, -1, -1):
+        finalList.val = listValues[i]
+        finalList = finalList.next
+    return
+
+
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        node = Node(val)
+        if not self.head:
+            self.head = node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.val, end="-->")
+            temp = temp.next
+
+
+lnkList = LinkedList()
+lnkList.append(1)
+lnkList.append(2)
+lnkList.append(3)
+lnkList.append(4)
+print("initial")
+lnkList.display()
+
+answer = reverseList(lnkList.head)
+print("\nfinal")
+lnkList.display()
--- tracker.js
+++ tracker.js
@@ -87,4 +87,33 @@ const todo = {
   stack_queue: {
     hard: ["84_largest_rectangle_in_histogram"],
   },
+  linked_list: {
+    easy: [
+      "206_reverse_linked_list",
+      "21_merge_two_sorted_lists",
+      "141_linked_list_cycle",
+      "83_remove_duplicates_from_sorted_list",
+      "234_palindrome_linked_list",
+      "203_remove_linked_list_elements",
+    ],
+    medium: [
+      "2_add_two_numbers",
+      "19_remove_nth_node_from_end",
+      "24_swap_nodes_in_pairs",
+      "61_rotate_list",
+      "82_remove_duplicates_from_sorted_list_II",
+      "86_partition_list",
+      "142_linked_list_cycle_II", //fast and slow pointer
+      "876_middle_of_linked_list", // fast and slow pointer
+      "92_reverse_linked_list_II", // reversal variations
+      "143_reorder_list", // reversal variations
+      "328_odd_even_linked_list", // structure / ordering
+      "147_insertion_sort_list", // structure / ordering
+    ],
+    hard: [
+      "23_merge_k_sorted_lists",
+      "25_reverse_nodes_in_k_group",
+      "138_copy_list_with_random_pointer",
+    ],
+  },
 };
